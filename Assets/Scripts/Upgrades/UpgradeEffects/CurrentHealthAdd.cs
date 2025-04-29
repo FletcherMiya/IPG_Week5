b3bc29c8b25504b85be571f15e75bc4be6c9415f
XiRdnethFr <yg2540@nyu.edu>
2025-04-29 17:11:09 -0400

    upgrade fixes

---
+++ Assets/Scripts/Upgrades/UpgradeEffects/CurrentHealthAdd.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This file contains all the upgrades.
+
+[CreateAssetMenu(menuName = "Upgrades/Effects/Current Health +1")]
+public class CurrentHealthUpgrade : UpgradeEffectBase
+{
+    public float healthRecover = 1f;
+
+    public override void Apply(GameObject player)
+    {
+        var stats = player.GetComponent<Player>().stats;
+        if (stats != null)
+        {
+            stats.health = Mathf.Min(stats.health + healthRecover, stats.maxHealth);
+        }
+    }
+}
---
+++ Assets/Scripts/Upgrades/UpgradeEffects/SpeedAdd.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This file contains all the upgrades.
+
+[CreateAssetMenu(menuName = "Upgrades/Effects/Max Speed +1")]
+public class MaxSpeedUpgrade : UpgradeEffectBase
+{
+    public float speedIncrease = 1f;
+
+    public override void Apply(GameObject player)
+    {
+        var stats = player.GetComponent<Player>().stats;
+        if (stats != null)
+        {
+            stats.speed += speedIncrease;
+        }
+    }
+}
